be84e7a004805231ca1dfbe35348c2427c4daa30
judel fintch <judfintch@gmail.com>
2021-11-17 07:47:22 +0100

    remove all text template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,18 +58,18 @@
                       <!-- ***** Logo End ***** -->
                       <!-- ***** Menu Start ***** -->
                       <ul class="nav">
-                          <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
-                          <li><a href="">Meetings</a></li>
-                          <li class="scroll-to-section"><a href="#apply">Apply Now</a></li>
+                          <li class="scroll-to-section"><a href="#top" class="active">Acceuil</a></li>
+                          <li><a href="">Apropos</a></li>
+                          <li class="scroll-to-section"><a href="#apply">Activités</a></li>
                           <li class="has-sub">
-                              <a href="javascript:void(0)">Pages</a>
+                              <a href="javascript:void(0)">SECTIONS</a>
                               <ul class="sub-menu">
-                                  <li><a href="">Upcoming Meetings</a></li>
-                                  <li><a href="">Meeting Details</a></li>
+                                  <li><a href=""></a></li>
+                                  <li><a href=""></a></li>
                               </ul>
                           </li>
-                          <li class="scroll-to-section"><a href="#courses">Courses</a></li> 
-                          <li class="scroll-to-section"><a href="#contact">Contact Us</a></li> 
+                          <li class="scroll-to-section"><a href="#courses">Galerie</a></li> 
+                          <li class="scroll-to-section"><a href="#contact">Contact</a></li> 
                       </ul>        
                       <a class='menu-trigger'>
                           <span>Menu</span>
