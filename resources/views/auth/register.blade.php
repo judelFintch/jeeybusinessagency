6f99a985af374120e8c6c4d3a28f64b6abe06a66
Scott Tresor <scotttresor@gmail.com>
2021-11-20 20:23:58 +0100

    [FEAT] review login and register  blade pages

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,127 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+
+@section('title')
+    AUTHENTIFICATION
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
+    <div class="nk-wrap nk-wrap-nosidebar">
+        <div class="nk-content ">
+            <div class="nk-split nk-split-page nk-split-md">
+                <div class="nk-split-content nk-block-area nk-block-area-column nk-auth-container bg-white">
+                    <div class="absolute-top-right d-lg-none p-3 p-sm-5">
+                        <a href="#" class="toggle btn-white btn btn-icon btn-light" data-target="athPromo"><em class="icon ni ni-info"></em></a>
+                    </div>
+                    <div class="nk-block nk-block-middle nk-auth-body">
+                        <div class="brand-logo pb-5 pt-4">
+                            <a href="{{ route('home.index') }}" class="logo-link">
+                                <img src="{{ asset('assets/images/logo.png') }}" alt="logo" height="90%" width="50%">
+                            </a>
+                        </div>
+                        <div class="nk-block-head">
+                            <div class="nk-block-head-content">
+                                <h5 class="nk-block-title">Connexion</h5>
+                            </div>
+                        </div>
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <div class="form-group">
+                                <div class="form-label-group">
+                                    <label class="form-label" for="name">Nom</label>
+                                </div>
+                                <input
+                                    type="text"
+                                    class="form-control form-control-lg @error('name') error @enderror"
+                                    id="name"
+                                    placeholder="Nom de l'utilisateur"
+                                    name="name"
+                                    value="{{ old('name') }}"
+                                    required
+                                    autocomplete="name"
+                                    autofocus
+                                >
                                 @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
+                            <div class="form-group">
+                                <div class="form-label-group">
+                                    <label class="form-label" for="email">Email</label>
+                                </div>
+                                <input
+                                    type="email"
+                                    class="form-control form-control-lg @error('email') error @enderror"
+                                    id="email"
+                                    name="email"
+                                    value="{{ old('email') }}"
+                                    required
+                                    autocomplete="email"
+                                    autofocus
+                                    placeholder="Adresse email de l'utilisateur"
+                                >
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
+                            <div class="form-group">
+                                <div class="form-label-group">
+                                    <label class="form-label" for="password">Mot de passe</label>
+                                </div>
+                                <div class="form-control-wrap">
+                                    <input
+                                        type="password"
+                                        class="form-control form-control-lg @error('password') error @enderror"
+                                        id="password"
+                                        placeholder="Enter your password"
+                                        name="password"
+                                        required
+                                        autocomplete="new-password"
+                                    >
+                                    @error('password')
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <strong>{{ $message }}</strong>
+                            </span>
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            <input type="hidden" value="avatar3.png" name="picture">
+                            <div class="form-group">
+                                <div class="form-label-group">
+                                    <label class="form-label" for="password-confirm">Mot de passe (Confirmation)</label>
+                                </div>
+                                <div class="form-control-wrap">
+                                    <input
+                                        type="password"
+                                        class="form-control form-control-lg @error('password') error @enderror"
+                                        id="password-confirm"
+                                        placeholder="Enter your password"
+                                        name="password_confirmation"
+                                        required
+                                        autocomplete="new-password"
+                                    >
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-lg btn-primary btn-block">S'inscrire</button>
                             </div>
+                        </form>
+                        <div class="form-note-s2 pt-4"> Already have an account ?
+                            <a href="{{ route('login') }}">connexion</a>
                         </div>
-                    </form>
+                    </div>
+                    <div class="nk-block nk-auth-footer">
+                        <div class="mt-3">
+                            <p>&copy; {{ now('Y')->format('Y') }} Fintchweb. Tout droit reserver.</p>
+                        </div>
+                    </div>
                 </div>
+                <div class="nk-split-content nk-split-stretch" style="background-image: url('{{ asset('assets/images/backend.jpg') }}'); background-position: center"></div>
             </div>
         </div>
     </div>
-</div>
 @endsection
