a37004da1beb5a47c26c669f7d9758503dbd9302
Scott Tresor <scotttresor@gmail.com>
2021-11-25 19:44:16 +0100

    [FIX] fix show pages if we wont get single article

--- resources/views/apps/partials/css.blade.php
+++ resources/views/apps/partials/css.blade.php
@@ -1,6 +1,5 @@
-
- <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="assets/css/fontawesome.css">
-<link rel="stylesheet" href="assets/css/templatemo-edu-meeting.css">
-<link rel="stylesheet" href="assets/css/owl.css">
-<link rel="stylesheet" href="assets/css/lightbox.css">
+<link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="{{ asset('assets/css/fontawesome.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/templatemo-edu-meeting.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/owl.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/lightbox.css') }}">
