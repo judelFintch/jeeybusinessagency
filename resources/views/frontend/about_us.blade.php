a5a4d78ef000175bfa5b4e4453529cfb413c5cd6
judel fintch <judfintch@gmail.com>
2021-12-07 20:42:02 +0100

    add text

--- resources/views/frontend/about_us.blade.php
+++ resources/views/frontend/about_us.blade.php
@@ -72,7 +72,9 @@
                 ;une école en progression à l’époque au niveau du secondaire où nous nous
                 sommes limités en 3e année secondaire, nous sommes allés progressivement 
                 pour présenter les premiers candidats aux examens d’états à l’année
-                scolaire 2013-2014 et cette fois-là nous avons fait le maximum <br>
+                scolaire 2013-2014 et cette fois-là nous avons fait le maximum 
+                </p>
+                <p>
                 de pourcentage aux examens d’états, fruit d’une formation meilleure.
                 Bien avant, notre école, dès la première année a été retenue comme centre du 
                 Test Nationale de Fin D’Etudes primaires (TENAFEP) jusqu’à ce jour et notre première année 
@@ -113,9 +115,14 @@
 
               <h3> DES  INFRASTRUCTURES  </h3>
            <p>
-           Notre complexe scolaire dispose de deux principaux bâtiments dont le premier bloc situé au numéro 3539 de l’avenue Kalombo au quartier Gambela II   abrite l’école maternelle et primaire tandis que le deuxième bloc sis croisement des avenues Kasangulu et de l’Eglise abrite l’école secondaire. Les deux imposants bâtiments sont construits en matériaux durables avec une architecture moderne offrant à cet effet une garantie de bonnes conditions d’études aux élèves qui y sont encadrés.
-
-Les images ci-après présentent l’apparence du bloc 1 de notre complexe scolaire qui comprend en tout 16 locaux  avec une capacité moyenne d’accueil de 50 élèves chacun dans lesquels fonctionnent les sections maternelle et primaire  plus un bâtiment administratif et un terrain de basketball et volleyball.
+           Notre complexe scolaire dispose de deux principaux bâtiments dont le premier
+            bloc situé au numéro 3539 de l’avenue Kalombo au quartier Gambela II 
+            abrite l’école maternelle et primaire tandis que le deuxième bloc 
+            sis croisement des avenues Kasangulu et de l’Eglise abrite l’école secondaire. Les deux imposants bâtiments sont construits en matériaux durables avec une architecture moderne offrant à cet effet une garantie de bonnes conditions d’études aux élèves qui y sont encadrés.
+            Les images ci-après présentent l’apparence du bloc 1 de notre complexe scolaire
+            qui comprend en tout 16 locaux  avec une capacité moyenne d’accueil
+            de 50 élèves chacun dans lesquels fonctionnent les sections maternelle
+             et primaire  plus un bâtiment administratif et un terrain de basketball et volleyball.
 </b>
               </p>
         </div>
@@ -133,12 +140,15 @@ Les images ci-après présentent l’apparence du bloc 1 de notre complexe scola
                 <p>
                 Notre souci est de pouvoir accompagner les autorités de notre pays à rendre effectif l’enseignement obligatoire pour tous les enfants ;pour atteindre cet objectif ,qu’il nous soit permis de lancer un vibrant  appel au gouvernement de soutenir notre œuvre en la retenant parmi les écoles devant bénéficier des subventions de l’Etat dans le cadre de la politique de gratuité de l’enseignement primaire et secondaire tel que prescrit dans notre constitution.
                     <br> <br>
-                    Notre souci est de pouvoir accompagner les autorités de notre pays à rendre effectif l’enseignement obligatoire pour tous les enfants ;pour atteindre cet objectif ,qu’il nous soit permis de lancer un vibrant  appel au gouvernement de soutenir notre œuvre en la retenant parmi les écoles devant bénéficier des subventions de l’Etat dans le cadre de la politique de gratuité de l’enseignement primaire et secondaire tel que prescrit dans notre constitution. 
-
-Ayant appris avec satisfaction le désir et la volonté du gouvernement de rendre la gratuite de l’enseignement primaire et secondaire effective dès l’année scolaire 2019 – 2020, nous avons souhaité nous inscrire dans cette initiative combien louable parmi les écoles privées qui seront retenues moyennant bien sûr une subvention de l’Etat en vue de soutenir l’action du gouvernement congolais.
-
-Le complexe scolaire NSANGA LE THANZIE a une capacité d’accueil  de 4000 élèves toutes sections confondues. Ces élèves devront suivre régulièrement les cours moyennant  les contributions financières annuelles de leurs parents qui s’élèvent à 350 $ par élève.
-En accueillant 4000 élèves chaque année, le budget annuel pour le paiement des rémunérations des enseignants et le personnel administratif sans oublier les autres frais de fonctionnement avoisine un montant de 140000$ dont le complexe a besoin pour son fonctionnement régulier.  
+                    Notre souci est de pouvoir accompagner les autorités de notre pays à rendre effectif l’enseignement obligatoire pour tous les enfants ;pour atteindre cet objectif ,qu’il nous soit permis de lancer un vibrant  appel au gouvernement de soutenir notre œuvre en la retenant parmi les écoles devant bénéficier des subventions de l’Etat dans le cadre de la politique de gratuité 
+                    de l’enseignement primaire et secondaire tel que prescrit dans notre constitution. 
+                    <p>
+                    Ayant appris avec satisfaction le désir et la volonté du gouvernement de rendre la gratuite de l’enseignement 
+                    primaire et secondaire effective dès l’année scolaire 2019 – 2020, nous avons souhaité nous inscrire dans cette initiative combien louable parmi les écoles privées qui seront retenues moyennant bien sûr une subvention de l’Etat en vue de soutenir l’action du gouvernement congolais.
+                    Le complexe scolaire NSANGA LE THANZIE a une capacité d’accueil  de 4000 élèves toutes sections confondues.
+                    <p>
+                     Ces élèves devront suivre régulièrement les cours moyennant  les contributions financières annuelles de leurs parents qui s’élèvent à 350 $ par élève.
+                    En accueillant 4000 élèves chaque année, le budget annuel pour le paiement des rémunérations des enseignants et le personnel administratif sans oublier les autres frais de fonctionnement avoisine un montant de 140000$ dont le complexe a besoin pour son fonctionnement régulier.  
 
                   </p>
 
@@ -165,6 +175,20 @@ En accueillant 4000 élèves chaque année, le budget annuel pour le paiement de
                   <br> <b>Section pédagogique générale</b>
                   <br> <b>Section coupe et couture</b>
                   <br><b>Section technique, options électricité et mécanique générale</b>
+              </p>
+
+              <h3>  DE LA TENUE DES ELEVES</h3>
+           <p>
+          La discipline, la morale et la bonne éducation se manifestent aussi à travers les accoutrements
+          de nos élèves qui sont suffisamment instruits sur la manière de s’habiller décemment 
+          chaque fois qu’ils se rendent à l’école et même en dehors de celle-ci.
+          Nos élèves filles sont facilement reconnaissables par leurs longues jupes et
+          leurs chemises manches longues couvrant tout le corps et les garçons en font de 
+          même avec des pantalons et chemises très confortables.
+              </p>
+              <h3>  DE LA VIE DE PRIERE</h3>
+           <p>
+           Les élèves qui fréquentent notre complexe sont également conviés à une vie de prière et de consécration dans le but de confier leur travail et leur avenir entre les mains de l’Eternel Dieu. C’est ainsi qu’il est organisé une dévotion matinale chaque jour au sein de notre école avant de s’atteler à toutes les autres taches de la journée..
               </p>
         </div>
         </div>
