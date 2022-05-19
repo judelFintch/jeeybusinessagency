b978c1c973caf95b35f72eb1229406eddc95d99a
judel fintch <judfintch@gmail.com>
2022-05-19 06:58:26 +0100

    adding shot text pages

--- resources/views/frontend/bloc_deux.blade.php
+++ resources/views/frontend/bloc_deux.blade.php
@@ -4,5 +4,33 @@
     ACCEUIL
 @endsection
 @section('content')
+
+<main id="main">
+
+<!-- ======= Breadcrumbs ======= -->
+<section id="breadcrumbs" class="breadcrumbs">
+  <div class="container">
+
+    <ol>
+      <li><a href="/">Acceuil</a></li>
+      <li>Apropos</li>
+    </ol>
+    <h2>Bloc 2</h2>
+
+  </div>
+</section><!-- End Breadcrumbs -->
+
+<!-- ======= Pricing Section ======= -->
+<section id="pricing" class="pricing">
+  <div class="container">
+
+    <div class="row no-gutters">
+
+    </div>
+  </div>
+</section><!-- End Pricing Section -->
+
+</main><!-- End #main -->
+
   
 @endsection
--- resources/views/frontend/visionnaire.blade.php
+++ resources/views/frontend/visionnaire.blade.php
@@ -4,5 +4,33 @@
     ACCEUIL
 @endsection
 @section('content')
+
+<main id="main">
+
+<!-- ======= Breadcrumbs ======= -->
+<section id="breadcrumbs" class="breadcrumbs">
+  <div class="container">
+
+    <ol>
+      <li><a href="index.html">Acceuil</a></li>
+      <li>Apropos</li>
+    </ol>
+    <h2>Mot du promoteur</h2>
+
+  </div>
+</section><!-- End Breadcrumbs -->
+
+<!-- ======= Pricing Section ======= -->
+<section id="pricing" class="pricing">
+  <div class="container">
+
+    <div class="row no-gutters">
+
+    </div>
+  </div>
+</section><!-- End Pricing Section -->
+
+</main><!-- End #main -->
+
   
 @endsection
