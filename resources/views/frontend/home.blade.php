c5d8e1c9f35b29bbd4d60c3ef68e42977fa5a758
judel fintch <judfintch@gmail.com>
2022-07-21 18:13:08 +0100

    add text describ

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -105,7 +105,7 @@
                         <div class="container">
                             <div class="row justify-content-center">
                                 <div class="col-12 col-lg-10">
-                                    <h5 class="text-light text-uppercase mb-3 animated slideInDown">Welcome to Apex</h5>
+                                    <h5 class="text-light text-uppercase mb-3 animated slideInDown">Welcome to Jeey Business</h5>
                                     <h1 class="display-2 text-light mb-3 animated slideInDown">A Construction & Renovation Company</h1>
                                     <ol class="breadcrumb mb-4 pb-2">
                                         <li class="breadcrumb-item fs-5 text-light">Commercial</li>
@@ -124,7 +124,7 @@
                         <div class="container">
                             <div class="row justify-content-center">
                                 <div class="col-12 col-lg-10">
-                                    <h5 class="text-light text-uppercase mb-3 animated slideInDown">Welcome to Apex</h5>
+                                    <h5 class="text-light text-uppercase mb-3 animated slideInDown">Welcome to Jeey Business</h5>
                                     <h1 class="display-2 text-light mb-3 animated slideInDown">Professional Tiling & Painting Services</h1>
                                     <ol class="breadcrumb mb-4 pb-2">
                                         <li class="breadcrumb-item fs-5 text-light">Commercial</li>
