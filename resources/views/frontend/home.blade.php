bf90dcd7b58bdd12778deebd65ebcfbfa0b90a37
judel fintch <judfintch@gmail.com>
2022-08-14 02:04:08 +0200

    fix assets

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -14,6 +14,9 @@
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&family=Poppins:wght@600;700&display=swap" rel="stylesheet"> 
 
     <!-- Icon Font Stylesheet -->
@@ -107,7 +110,7 @@
                             <div class="row justify-content-center">
                                 <div class="col-12 col-lg-10">
                                     <h5 class="text-light text-uppercase mb-3 animated slideInDown">Bienvenue chez Jeey Business Agency</h5>
-                                    <h1 class="display-2 text-light mb-3 animated slideInDown">A Construction & Immobilier Company</h1>
+                                    <h1 class="display-2 text-light mb-3 animated slideInDown">Une entreprise de construction et d'immobilier</h1>
                                     <ol class="breadcrumb mb-4 pb-2">
                                         <li class="breadcrumb-item fs-5 text-light">Commerciale</li>
                                         <li class="breadcrumb-item fs-5 text-light">Residentiele</li>
@@ -173,7 +176,7 @@
                 <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
                     <div class="h-100">
                         <div class="border-start border-5 border-primary ps-4 mb-5">
-                            <h6 class="text-body text-uppercase mb-2">About Us</h6>
+                            <h6 class="text-body text-uppercase mb-2">Apropos</h6>
                             <h1 class="display-6 mb-0">Unique Solutions For Residentials & Industries!</h1>
                         </div>
                         <p>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
@@ -221,7 +224,7 @@
                     <img class="img-fluid w-100" src="{{asset('assets/img/fact-2.jpg')}}" alt="">
                     <div class="facts-overlay">
                         <h1 class="display-1">02</h1>
-                        <h4 class="text-white mb-3">Mechanical</h4>
+                        <h4 class="text-white mb-3">Immobilier</h4>
                         <p class="text-white">Aliqu diam amet diam et eos erat ipsum lorem stet lorem sit clita duo justo erat amet</p>
                         <a class="text-white small" href="">READ MORE<i class="fa fa-arrow-right ms-3"></i></a>
                     </div>
@@ -243,7 +246,7 @@
                     <img class="img-fluid w-100" src="{{asset('assets/img/fact-4.jpg')}}" alt="">
                     <div class="facts-overlay">
                         <h1 class="display-1">04</h1>
-                        <h4 class="text-white mb-3">Interior Design</h4>
+                        <h4 class="text-white mb-3">Location</h4>
                         <p class="text-white">Aliqu diam amet diam et eos erat ipsum lorem stet lorem sit clita duo justo erat amet</p>
                         <a class="text-white small" href="">READ MORE<i class="fa fa-arrow-right ms-3"></i></a>
                     </div>
@@ -554,8 +557,8 @@
         <div class="container py-5">
             <div class="row g-5">
                 <div class="col-lg-3 col-md-6">
-                    <h1 class="text-white mb-4"><i class="fa fa-building text-primary me-3"></i>APEX</h1>
-                    <p>Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita</p>
+                    <h1 class="text-white mb-4"><i class="fa fa-building text-primary me-3"></i>JBA</h1>
+                    <p>jeeybusinessagency est une S.A.R.L congolaise basée à Lubumbashi (RDC). JBA est représentée par Deux sièges à Kolwezi et Lubumbash</p>
                     <div class="d-flex pt-2">
                         <a class="btn btn-square btn-outline-primary me-1" href=""><i class="fab fa-twitter"></i></a>
                         <a class="btn btn-square btn-outline-primary me-1" href=""><i class="fab fa-facebook-f"></i></a>
@@ -565,17 +568,17 @@
                 </div>
                 <div class="col-lg-3 col-md-6">
                     <h4 class="text-light mb-4">Address</h4>
-                    <p><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-                    <p><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-                    <p><i class="fa fa-envelope me-3"></i>info@example.com</p>
+                    <p><i class="fa fa-map-marker-alt me-3"></i>Haut Katanga, Lubumbashi, Av Rouwe coin Maniema</p>
+                    <p><i class="fa fa-phone-alt me-3"></i>+243 97 054 63 11</p>
+                    <p><i class="fa fa-envelope me-3"></i>inf@jeeybusinessagency.com</p>
                 </div>
                 <div class="col-lg-3 col-md-6">
                     <h4 class="text-light mb-4">Quick Links</h4>
-                    <a class="btn btn-link" href="">About Us</a>
-                    <a class="btn btn-link" href="">Contact Us</a>
-                    <a class="btn btn-link" href="">Our Services</a>
-                    <a class="btn btn-link" href="">Terms & Condition</a>
-                    <a class="btn btn-link" href="">Support</a>
+                    <a class="btn btn-link" href="">Apropos</a>
+                    <a class="btn btn-link" href="">Equipes</a>
+                    <a class="btn btn-link" href="">Nos Services</a>
+                    <a class="btn btn-link" href="">Contact</a>
+                    
                 </div>
                 <div class="col-lg-3 col-md-6">
                     <h4 class="text-light mb-4">Newsletter</h4>
@@ -591,11 +594,11 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        &copy; <a href="#">Your Site Name</a>, All Right Reserved.
+                        &copy; <a href="#">jeeybusinessagency</a>, All Right Reserved.
                     </div>
                     <div class="col-md-6 text-center text-md-end">
                         <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                        Designed By <a href="https://htmlcodex.com">HTML Codex</a>
+                        Designed By Judel Fintch </a>
                     </div>
                 </div>
             </div>
@@ -609,14 +612,17 @@
 
 
     <!-- JavaScript Libraries -->
-    
-    <script src="{{asset('assets/lib/wow/wow.min.js')}}"></script>
-    <script src="{{asset('assets/lib/easing/easing.min.js')}}"></script>
-    <script src="{{asset('assets/lib/waypoints/waypoints.min.js')}}"></script>
-    <script src="{{asset('assets/lib/owlcarousel/owl.carousel.min.js')}}"></script>
+ <
 
     <!-- Template Javascript -->
-    <script src="assets/js/main.js"></script>
+   <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('assets/lib/wow/wow.min.js')}}"></script>
+    <script src="{{ asset('assets/lib/easing/easing.min.js')}}"></script>
+    <script src="{{ asset('assets/lib/waypoints/waypoints.min.js')}}"></script>
+    <script src="{{ asset('assets/lib/owlcarousel/owl.carousel.min.js')}}"></script>
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
 </body>
 
 </html>
