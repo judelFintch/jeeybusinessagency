187dcc3a59cd835c8e0046faea97fc0ad3db12b1
judel fintch <judfintch@gmail.com>
2022-08-19 11:26:56 +0200

    refector codes

---
+++ resources/views/apps/partials/app.blade.php
@@ -0,0 +1,120 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Jeey Business Agency</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&family=Poppins:wght@600;700&display=swap" rel="stylesheet"> 
+
+    <!-- Icon Font Stylesheet -->
+   
+    <!-- Libraries Stylesheet -->
+    <link href="{{asset('assets/lib/animate/animate.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+</head>
+
+<body>
+    @yield('content')
+
+<!-- Footer Start -->
+<div class="container-fluid bg-dark footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-3 col-md-6">
+                    <h1 class="text-white mb-4"><i class="fa fa-building text-primary me-3"></i>JBA</h1>
+                    <p>jeeybusinessagency est une S.A.R.L congolaise basée à Lubumbashi (RDC). JBA est représentée par Deux sièges à Kolwezi et Lubumbash</p>
+                    <div class="d-flex pt-2">
+                        <a class="btn btn-square btn-outline-primary me-1" href=""><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-square btn-outline-primary me-1" href=""><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-square btn-outline-primary me-1" href=""><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-square btn-outline-primary me-0" href=""><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-light mb-4">Addresse</h4>
+                    <p><i class="fa fa-map-marker-alt me-3"></i>Haut Katanga, Lubumbashi, Av Rouwe coin Maniema</p>
+                    <p><i class="fa fa-phone-alt me-3"></i>+243 97 054 63 11</p>
+                    <p><i class="fa fa-envelope me-3"></i>info@jeeybusinessagency.com</p>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-light mb-4">Quick Links</h4>
+                    <a class="btn btn-link" href="">Apropos</a>
+                    <a class="btn btn-link" href="">Equipes</a>
+                    <a class="btn btn-link" href="">Nos Services</a>
+                    <a class="btn btn-link" href="">Contact</a>
+                    
+                </div>
+                
+            </div>
+        </div>
+        <div class="container-fluid copyright">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                        &copy; <a href="#">jeeybusinessagency</a>, All Right Reserved.
+                    </div>
+                    <div class="col-md-6 text-center text-md-end">
+                        <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+                        Designed By Judel Fintch </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+ <
+
+    <!-- Template Javascript -->
+   <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('assets/lib/wow/wow.min.js')}}"></script>
+    <script src="{{ asset('assets/lib/easing/easing.min.js')}}"></script>
+    <script src="{{ asset('assets/lib/waypoints/waypoints.min.js')}}"></script>
+    <script src="{{ asset('assets/lib/owlcarousel/owl.carousel.min.js')}}"></script>
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/62ff50ca37898912e963ed31/1gaql7660';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,44 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <title>Jeey Business Agency</title>
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="" name="keywords">
-    <meta content="" name="description">
-
-    <!-- Favicon -->
-    <link href="img/favicon.ico" rel="icon">
-
-    <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&family=Poppins:wght@600;700&display=swap" rel="stylesheet"> 
-
-    <!-- Icon Font Stylesheet -->
-   
-    <!-- Libraries Stylesheet -->
-    <link href="{{asset('assets/lib/animate/animate.min.css')}}" rel="stylesheet">
-    <link href="{{asset('assets/lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
-
-    <!-- Customized Bootstrap Stylesheet -->
-    <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
-
-    <!-- Template Stylesheet -->
-    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
-</head>
-
-<body>
-    <!-- Spinner Start -->
-   
-    <!-- Spinner End -->
-
-
-    <!-- Topbar Start -->
+@extends('apps.partials.app')
+ @section('content')
     <div class="container-fluid bg-light p-0">
         <div class="row gx-0 d-none d-lg-flex">
             <div class="col-lg-7 px-5 text-start">
@@ -57,7 +18,7 @@
             </div>
             <div class="col-lg-5 px-5 text-end">
                 <div class="h-100 d-inline-flex align-items-center">
-                    <a class="btn btn-square border-end border-start" href=""><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-square border-end border-start" href="https://web.facebook.com/tony.ndola9010?_rdc=1&_rdr"><i class="fab fa-facebook-f"></i></a>
                     <a class="btn btn-square border-end" href=""><i class="fab fa-twitter"></i></a>
                     <a class="btn btn-square border-end" href=""><i class="fab fa-linkedin-in"></i></a>
                     <a class="btn btn-square border-end" href=""><i class="fab fa-instagram"></i></a>
@@ -166,7 +127,7 @@
                         <img class="position-absolute w-100 h-100" src="{{asset('assets/img/about.jpg')}}" alt="" style="object-fit: cover;">
                         <div class="position-absolute top-0 start-0 bg-white pe-3 pb-3" style="width: 200px; height: 200px;">
                             <div class="d-flex flex-column justify-content-center text-center bg-primary h-100 p-3">
-                                <h1 class="text-white">2</h1>
+                                <h1 class="text-white">4</h1>
                                 <h2 class="text-white">Ans</h2>
                                 <h5 class="text-white mb-0">Experiences</h5>
                             </div>
@@ -181,7 +142,7 @@
                         </div>
                         <p>La Société JBA est spécialisée dans la construction de routes, ponts, constructions immobilières, ingénierie municipale, travaux hydrauliques et électriques, structures métalliques et autres infrastructures à grande échelle de construction et d’exploitation minière.
 
-Jeeybusinessagency (JBA en sigle), fondé en 2021 en Republique Democratique Du Congo est présent dans plusieurs Province, tel Kinshasa, Lubumbash et Kolwezi.
+Jeeybusinessagency (JBA en sigle), fondé en 2021 en Republique Democratique Du Congo est présent dans plusieurs Province, tel Kinshasa, Lubumbashi, Kolwezi et Goma.
 
 </p>
                         <p class="mb-4">JBA entreprend des projets variés, équipé de machines complètes et d’équipements avancés, grâce à sa forte capacité, sa qualité et son excellente réputation.
@@ -313,9 +274,9 @@ Notre passion commune : réaliser les projets que vous imaginez.
                         <img class="position-absolute w-100 h-100" src="{{asset('assets/img/feature.jpg')}}" alt="" style="object-fit: cover;">
                         <div class="position-absolute top-0 start-0 bg-white pe-3 pb-3" style="width: 200px; height: 200px;">
                             <div class="d-flex flex-column justify-content-center text-center bg-primary h-100 p-3">
-                                <h1 class="text-white">25</h1>
-                                <h2 class="text-white">Years</h2>
-                                <h5 class="text-white mb-0">Experience</h5>
+                                <h1 class="text-white">4</h1>
+                                <h2 class="text-white">Ans</h2>
+                                <h5 class="text-white mb-0">Experiences</h5>
                             </div>
                         </div>
                     </div>
@@ -481,115 +442,9 @@ Notre passion commune : réaliser les projets que vous imaginez.
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-7 wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="owl-carousel testimonial-carousel">
-                        <div class="testimonial-item">
-                            <img class="img-fluid mb-4" src="{{asset('assets/img/testimonial-1.jpg')}}" alt="">
-                            <p class="fs-5">.</p>
-                            <div class="bg-primary mb-3" style="width: 60px; height: 5px"></div>
-                            <h5>Client Name</h5>
-                            <span>Profession</span>
-                        </div>
-                        <div class="testimonial-item">
-                            <img class="img-fluid mb-4" src="img/testimonial-2.jpg" alt="">
-                            <p class="fs-5">Dolores sed duo clita tempor justo dolor et stet lorem kasd labore dolore lorem ipsum. At lorem lorem magna ut et, nonumy et labore et tempor diam tempor erat.</p>
-                            <div class="bg-primary mb-3" style="width: 60px; height: 5px"></div>
-                            <h5>Client Name</h5>
-                            <span>Profession</span>
-                        </div>
-                    </div>
-                </div>
+                
             </div>
         </div>
     </div>
     <!-- Testimonial End -->
-
-
-    <!-- Footer Start -->
-    <div class="container-fluid bg-dark footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
-        <div class="container py-5">
-            <div class="row g-5">
-                <div class="col-lg-3 col-md-6">
-                    <h1 class="text-white mb-4"><i class="fa fa-building text-primary me-3"></i>JBA</h1>
-                    <p>jeeybusinessagency est une S.A.R.L congolaise basée à Lubumbashi (RDC). JBA est représentée par Deux sièges à Kolwezi et Lubumbash</p>
-                    <div class="d-flex pt-2">
-                        <a class="btn btn-square btn-outline-primary me-1" href=""><i class="fab fa-twitter"></i></a>
-                        <a class="btn btn-square btn-outline-primary me-1" href=""><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-square btn-outline-primary me-1" href=""><i class="fab fa-youtube"></i></a>
-                        <a class="btn btn-square btn-outline-primary me-0" href=""><i class="fab fa-linkedin-in"></i></a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-light mb-4">Address</h4>
-                    <p><i class="fa fa-map-marker-alt me-3"></i>Haut Katanga, Lubumbashi, Av Rouwe coin Maniema</p>
-                    <p><i class="fa fa-phone-alt me-3"></i>+243 97 054 63 11</p>
-                    <p><i class="fa fa-envelope me-3"></i>inf@jeeybusinessagency.com</p>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-light mb-4">Quick Links</h4>
-                    <a class="btn btn-link" href="">Apropos</a>
-                    <a class="btn btn-link" href="">Equipes</a>
-                    <a class="btn btn-link" href="">Nos Services</a>
-                    <a class="btn btn-link" href="">Contact</a>
-                    
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-light mb-4">Newsletter</h4>
-                    <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-                    <div class="position-relative mx-auto" style="max-width: 400px;">
-                        <input class="form-control bg-transparent w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
-                        <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="container-fluid copyright">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        &copy; <a href="#">jeeybusinessagency</a>, All Right Reserved.
-                    </div>
-                    <div class="col-md-6 text-center text-md-end">
-                        <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                        Designed By Judel Fintch </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Footer End -->
-
-
-    <!-- Back to Top -->
-    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
-
-
-    <!-- JavaScript Libraries -->
- <
-
-    <!-- Template Javascript -->
-   <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('assets/lib/wow/wow.min.js')}}"></script>
-    <script src="{{ asset('assets/lib/easing/easing.min.js')}}"></script>
-    <script src="{{ asset('assets/lib/waypoints/waypoints.min.js')}}"></script>
-    <script src="{{ asset('assets/lib/owlcarousel/owl.carousel.min.js')}}"></script>
-    <!-- Template Javascript -->
-    <script src="js/main.js"></script>
-</body>
-
-</html>
-
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/62ff50ca37898912e963ed31/1gaql7660';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
+@endsection
