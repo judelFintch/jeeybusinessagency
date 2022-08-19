aba9c646b1148a9738cc458f912d2a9dc45f5a57
judel fintch <judfintch@gmail.com>
2022-08-19 11:03:04 +0200

    adding chatcall

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -579,3 +579,17 @@ Notre passion commune : réaliser les projets que vous imaginez.
 </body>
 
 </html>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/62ff50ca37898912e963ed31/1gaql7660';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
